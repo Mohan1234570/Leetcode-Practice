32f724ee103ff2110fd44c6236c5d6b0519678d5
GUDUMALA <119730117+Mohan1234570@users.noreply.github.com>
2025-12-01 18:33:07 +0530

    Time: 26 ms (44.78%) | Memory: 44.3 MB (15.15%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -20,23 +20,21 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        if(node == null){
-            return null;
-        }
-        HashMap<Node,Node> vis = new HashMap<>();
-        return clonedGraph(node,vis);
+      if (node == null) return null;
+      Map<Node,Node> map = new HashMap<>();
+      return dfs(map,node);
     }
 
-    public Node clonedGraph(Node node, HashMap<Node,Node> vis){
-        if(vis.containsKey(node)){
-            return vis.get(node);
+    public Node dfs(Map<Node,Node> map, Node node){
+        if(map.containsKey(node)){
+            return map.get(node);
         }
-
-        Node clone = new Node(node.val, new ArrayList<>());
-        vis.put(node,clone);
+        Node clone = new Node(node.val);
+        map.put(node,clone);
 
         for(Node n : node.neighbors){
-            clone.neighbors.add(clonedGraph(n,vis));
+                clone.neighbors.add(dfs(map,n));           
+
         }
 
         return clone;
